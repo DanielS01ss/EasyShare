8cb953e75663b4f95d580f8145eb3d3b30c2550c
Daniel Stanculescu <danystanculescu@gmail.com>
2022-07-18 14:48:18 +0300

    Added Google Authentication Button from Google

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
+import { useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import jwt_decode from "jwt-decode";
 import MainPage from "./Components/MainPage.js";
-import MainDashboardPage from './Components/MainDashboard/MainDashboardPage';
-import ProfilePage from './Components/ProfilePage';
+
+
 
 function App() {
-  return (
+
+
+  const [user, setUser] = useState({});
+
+  const handleSignOut =()=>{
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  ///aici vei face handlingul la logarea cu google 
+  /// vei primi un jwt si vei lua mailul , eventual poza si PENTRU CA SA AI UN ID PE BACKEND
+  // o sa decodezi jwt-ul pe care-l primesti si o sa iei parametrul sub
+  const handleCallbackResponse = (response)=>
+  {
+  
+    console.log("Encoded JWT ID token: "+response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+  
+
+  useEffect(()=>{
+    /* global google*/
+    google.accounts.id.initialize({
+      client_id:"231967582415-gd49ib3e0agjqa7knoo4o5d876bu779e.apps.googleusercontent.com",
+      callback:handleCallbackResponse
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInWithGoogle"),
+      {theme:"outline" , size:"large"}
+    );
+
+  },[]);
+
+  //no user sign in button
+  /// have user : show log out button
+
+  return ( 
     <div className="App">
-      <MainDashboardPage/>
+        <MainPage/>
+
     </div>
   );
 }
--- src/Components/Authentication-Components/LoginCard.js
+++ src/Components/Authentication-Components/LoginCard.js
@@ -12,7 +12,7 @@ const LoginCard = ()=>{
 			<h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
 			<div>
 				<label for="email" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your email</label>
-				<input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required=""/>
+				<input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@email.com" required=""/>
             </div>
 				<div>
 					<label for="password" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your password</label>
@@ -31,7 +31,8 @@ const LoginCard = ()=>{
 								Password?</a>
 						</div>
 						<button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex flex-row justify-evenly"><p>Login with Google </p><img className="w-6" src={GoogleLogo} alt="google logo"/></button>
+						<div id="signInWithGoogle"></div>
+                        {/* <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-14 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex flex-row justify-evenly"><p>Login with Google </p><img className="w-6" src={GoogleLogo} alt="google logo"/></button> */}
 						<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
 							Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create
 								account</a>
